0b05fee48d19ea0372b2b593a82dba79ec39346f
Pulak Bala <pulakbala0524@gmail.com>
2023-04-17 00:15:21 +0600

    modify

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -52,7 +52,7 @@ const Skills = () => {
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-16 py-20 px-20  items-center justify-center">
-            <div className="border  py-10 bg-slate-900 rounded-full">
+            <div className="border  py-10 bg-slate-900 md:rounded-full rounded-lg">
             <div className="flex justify-center justify-around text-2xl pb-9">
               <h2 className="border bg-cyan-600 font-bold p-2">HTML</h2>
               <h2 className="border bg-cyan-600 p-2 rounded-lg">100%</h2>
@@ -60,7 +60,7 @@ const Skills = () => {
             <progress className="progress progress-info w-8/12" value="100" max="100"></progress>
             </div>
 
-            <div className="border  py-10 bg-slate-900 rounded-full">
+            <div className="border  py-10 bg-slate-900 md:rounded-full rounded-lg">
             <div className="flex justify-center justify-around text-2xl pb-9">
               <h2 className="border bg-cyan-600 font-bold p-2">CSS</h2>
               <h2 className="border bg-cyan-600 p-2 rounded-lg">85%</h2>
@@ -69,7 +69,7 @@ const Skills = () => {
             </div>
 
             
-            <div className="border  py-10 bg-slate-900 rounded-full ">
+            <div className="border  py-10 bg-slate-900 md:rounded-full rounded-lg ">
             <div className="flex  justify-center justify-around ml-5 text-2xl pb-9 w-11/12">
               <h2 className="border bg-cyan-600 font-bold p-2 rounded-lg">BOOTSTRAP</h2>
               <h2 className="border bg-cyan-600 p-2 rounded-lg ">8%</h2>
@@ -77,7 +77,7 @@ const Skills = () => {
             <progress className="progress progress-info w-8/12" value="80" max="100"></progress>
             </div>
 
-            <div className="border  py-10 bg-slate-900 rounded-full">
+            <div className="border  py-10 bg-slate-900 md:rounded-full rounded-lg">
             <div className="flex justify-center justify-around text-2xl pb-9">
               <h2 className="border bg-cyan-600 font-bold p-2">TAILWIND</h2>
               <h2 className="border bg-cyan-600 p-2 rounded-lg">90%</h2>
@@ -85,7 +85,7 @@ const Skills = () => {
             <progress className="progress progress-info w-8/12" value="90" max="100"></progress>
             </div>
 
-            <div className="border  py-10 bg-slate-900 rounded-full">
+            <div className="border  py-10 bg-slate-900 md:rounded-full rounded-lg">
             <div className="flex justify-center justify-around text-2xl pb-9">
               <h2 className="border bg-cyan-600 font-bold p-2">JS</h2>
               <h2 className="border bg-cyan-600 p-2 rounded-lg">70%</h2>
@@ -94,7 +94,7 @@ const Skills = () => {
             </div>
 
 
-            <div className="border  py-10 bg-slate-900 rounded-full">
+            <div className="border  py-10 bg-slate-900 md:rounded-full rounded-lg">
             <div className="flex justify-center justify-around text-2xl pb-9">
               <h2 className="border bg-cyan-600 font-bold p-2">REACT</h2>
               <h2 className="border bg-cyan-600 p-2 rounded-lg">75%</h2>
